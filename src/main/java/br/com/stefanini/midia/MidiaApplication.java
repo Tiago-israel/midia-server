14c70e8910f6c7835126ced42f71e1f0d19517e6
unknown <tiagoisrael77@hotmail.com>
2018-02-15 08:55:42 -0200

    adicionando controle de cach no projeto

--- src/main/java/br/com/stefanini/midia/MidiaApplication.java
+++ src/main/java/br/com/stefanini/midia/MidiaApplication.java
@@ -2,8 +2,11 @@ package br.com.stefanini.midia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
 
 @SpringBootApplication
+@EnableCaching
 public class MidiaApplication {
 
 	public static void main(String[] args) {
